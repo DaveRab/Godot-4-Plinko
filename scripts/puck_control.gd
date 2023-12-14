0c0b7feff6820e3de88f06ddedef001d88faf863
DaveRab <dc.rabideau@gmail.com>
2023-12-14 18:19:04 -0500

    WIP working on adding controller for puck (RigidBody2d) so player can move and release puck at game start

--- scripts/puck_control.gd
+++ scripts/puck_control.gd
@@ -1,5 +1,6 @@
-extends RigidBody2D
+extends Control
 
+@onready var puck = $Puck
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	puck.freeze = true
